802c8449d65ccc646011672ce2c59849eccbf08d
ygaurkar1311 <2022bit021@sggs.ac.in>
2024-01-29 12:27:19 +0530

    Task 2 24 Jan

--- SY_2022BIT021_Task2.java
+++ SY_2022BIT021_Task2.java
@@ -7,13 +7,13 @@ public class SY_2022BIT021_Task2{
 	
 	static boolean verifyRegistrationNumber(Character[] args){
 		for(int i=0;i<=args.length;i++){
-		if(i<=3||i>=7 && i<args.length){
+			if(i<=3||i>=7 && i<args.length){
 			boolean a=Character.isDigit(args[i]);
 				if(!a){
 				return false;
 				}
 			}
-		if(i>3 && i<7){
+			if(i>3 && i<7){
 			boolean b=Character.isAlphabetic(args[i]);
 				if(!b){
 				return false;
